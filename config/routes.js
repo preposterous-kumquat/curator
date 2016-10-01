cd0e206961affa54366af5fc5c3d8710bc810f7a
Brian Kilrain <bkilrain@gmail.com>
2016-09-30 19:41:30 -0700

    filter for points in one single direction

--- config/routes.js
+++ config/routes.js
@@ -14,20 +14,34 @@ module.exports = (app, express) => {
     console.log(req.query)
     let seedId = req.query.id;
     let seedTheme = req.query.theme;
-    client.hgetallAsync(`gps:${seedId}`).then( (data) => {
+    client.hgetallAsync(`gps:${seedId}`)
+    .then( (data) => {
       let oppLoc = {
         lat: data.lat > 0 ? -(data.lat) : Math.abs(data.lat),
         long: data.long > 0 ? -(180 - data.long) : 180 - Math.abs(data.long)
-      }
-      console.log(oppLoc)
+      };
+      console.log(data.long, oppLoc.long)
+      client.lrangeAsync(`list:${seedTheme}`, 0, 100)
+      .then( (list) => {
+        let oneDirectionPoints = {};
+        for (let i = 0; i < list.length; i += 3) {
+          if (list[i] > Math.min(data.long, oppLoc.long) && list[i] < Math.max(data.long, oppLoc.long)) {
+            oneDirectionPoints[list[i+2]] = {};
+            oneDirectionPoints[list[i+2]].lat = list[i+1];
+            oneDirectionPoints[list[i+2]].long = list[i];
+          }
+        }
+        console.log(oneDirectionPoints)
+      });
 
     });
+
     
   });
   app.post('/save', (req, res) => {
     let data = req.body;
     console.log('posting...', data)
-    client.sadd(`set:${data.theme}`, data.id);
+    client.lpush(`list:${data.theme}`, data.id, data.gps.lat, data.gps.long);
     
     client.hmset(`gps:${data.id}`, 'lat', data.gps.lat, 'long', data.gps.long, 'url', data.url);
     // TODO: CREATE SORTED SET BY TIME FOR PICTURES
